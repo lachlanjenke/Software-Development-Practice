623b598541b74d12bf3ca8bbbb87d890ab8d3b66
Lachlan Jenke <a1924627@adelaide.edu.au>
2026-03-06 11:56:37 +1030

    Created person class with auto properties + constructor

--- Topic 1/Person/Person.cs
+++ Topic 1/Person/Person.cs
@@ -2,8 +2,17 @@ using System;
 
 namespace Person
 {
-    class Person
+    public class Person
     {
-        
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        int Age { get; set; }
+
+        Person(string firstNameValue, string lastNameValue, int ageValue)
+        {
+            FirstName = firstNameValue;
+            LastName = lastNameValue;
+            Age = ageValue;
+        }
     }
 }
