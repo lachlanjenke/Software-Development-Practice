7a00ecc0c6a09663229c4852508c703f6da80691
Lachlan Jenke <a1924627@adelaide.edu.au>
2026-03-06 11:42:28 +1030

    Finished main as well

--- Topic 1/PayrollCalculator/Program.cs
+++ Topic 1/PayrollCalculator/Program.cs
@@ -8,7 +8,31 @@ namespace PayrollCalculator
         const float TAX_RATE = 0.2f;
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Console.Write("Enter Employee name: ");
+                string? name = Console.ReadLine();
+
+                Console.Write("Hours worked: ");
+                if (!double.TryParse(Console.ReadLine(), out double hours))
+                {
+                    throw new Exception("Invalid input for hours");
+                }
+
+                Console.Write("Hourly rate: ");
+                if (!double.TryParse(Console.ReadLine(), out double rate))
+                {
+                    throw new Exception("Invalid input for hours");
+                }
+
+                double net_pay = CalculatePay(hours, rate);
+
+                Console.WriteLine(name + " earned $" + $"{net_pay:F2}" + " after tax.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error has occured: {e.Message}");
+            }
         }
 
         static double CalculatePay(double hours, double rate)
