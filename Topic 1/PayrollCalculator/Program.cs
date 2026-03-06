579d92c9d1ba713e46aaae55de472f8b1fdca54c
Lachlan Jenke <a1924627@adelaide.edu.au>
2026-03-06 11:41:43 +1030

    Created the Calculate Pay Function

---
+++ Topic 1/PayrollCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace PayrollCalculator
+{
+    class Program
+    {
+        const float TAX_RATE = 0.2f;
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static double CalculatePay(double hours, double rate)
+        {
+            if (hours < 0 || rate < 0)
+            {
+                throw new ArgumentException("Hours and rate must be positive");
+            }
+            double gross, tax, net;
+
+            gross = hours * rate;
+            tax = gross * TAX_RATE;
+            net = gross - tax;
+
+            return net;
+        }
+    }
+}
