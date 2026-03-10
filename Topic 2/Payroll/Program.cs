c0ea8fa9a1abb54c52fc5dfc020e63f40434723a
Lachlan Graham Jenke (Student) <a1924627@adelaide.edu.au>
2026-03-10 12:38:57 +1030

    Turned the payroll function into its own class

---
+++ Topic 2/Payroll/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Payroll;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Payroll {
+    const double TAX_RATE = 0.2d;
+    private double hours;
+    private double rate;   
+
+    Payroll(double employeeHours, double employeeRate)
+    {
+        hours = employeeHours;
+        rate = employeeRate;
+    }
+
+    public double CalculatePay()
+    {
+        if(hours < 0 || rate < 0)
+        {
+            throw new ArgumentException("Hours and Rate must be positive");
+        }
+
+        double gross, tax, net;
+
+        gross = hours * rate;
+        tax = gross * TAX_RATE;
+        net = gross - tax;
+
+        return net;
+    }
+}
