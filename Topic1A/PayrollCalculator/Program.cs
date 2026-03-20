aa0de374e2e0c42bc238a15ba4303eb2bd8f3914
Lachlan Jenke <a1924627@adelaide.edu.au>
2026-03-20 16:03:28 +1030

    Converted payroll calculator to python

---
+++ Topic1A/PayrollCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace PayrollCalculator;
+
+class Program
+{
+    const double TAX_RATE = 0.2d;
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter Employee name: ");
+            string? name = Console.ReadLine();
+            
+            Console.Write("Hours worked: ");
+            if (!double.TryParse(Console.ReadLine(), out double hours))
+                throw new Exception("Invalid input for hours");
+  
+            Console.Write("Hourly rate: ");
+            if (!double.TryParse(Console.ReadLine(), out double rate))
+                throw new Exception("Invalid input for hours");
+
+            double net_pay = CalculatePay(hours, rate);
+
+            Console.WriteLine(name + " earned $" + $"{net_pay:F2}" + " after tax.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error has occured: {e.Message}");
+        }
+        
+    }
+
+    static double CalculatePay(double hours, double rate)
+    {
+        if (hours < 0 || rate < 0)
+            throw new ArgumentException("Hours and rate must be positive");
+        
+        double gross, tax, net;
+
+        gross = hours * rate;
+        tax = gross * TAX_RATE;
+        net = gross - tax;
+
+        return net;
+    }
+}
