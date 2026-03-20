7a9d2bc9a69c09e7c1a5bb526e26b48cb19f50db
Lachlan Jenke <a1924627@adelaide.edu.au>
2026-03-20 15:54:29 +1030

    Cleaning up repository

---
+++ Topic1A/IntroTopic/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace IntroTopic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string favColour = "Blue";
+        Console.WriteLine("Enter your favourite colour: ");
+        favColour = Console.ReadLine();
+        Console.WriteLine("Your favourite colour is " + favColour);
+    }
+}
