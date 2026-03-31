90c5ec59491b7d858d0a240791005a6349fdd703
Lachlan Jenke <a1924627@adelaide.edu.au>
2026-03-31 11:21:24 +1030

    Task 1 done

--- Topic5A/task1/Program.cs
+++ Topic5A/task1/Program.cs
@@ -6,7 +6,55 @@ class Program
     {
         string[] names = {"Noah", "Charlie","Oliver","Amelia","Theo","Mia","Henry","Evelyn","Lachy","Ava"};
 
+        // Print Array
         foreach (string name in names)
             Console.WriteLine(name);
+
+        // Find longest and shortest names
+        string shortestName, longestName;
+        shortestName = longestName = names[0];
+
+        foreach (string name in names)
+        {
+            // Check if Longest
+            if (name.Length > longestName.Length)
+                longestName = name;
+            // Check if Shortest
+            else if (name.Length < shortestName.Length)
+                shortestName = name;
+        }
+        Console.WriteLine("Longest is: " + longestName + ", Shorest is: " + shortestName);
+
+        // Print before Sorting
+        Console.WriteLine("Before sorting");
+        foreach (string name in names)
+            Console.Write(name + " ");
+            
+        Console.WriteLine(); // New line
+
+        Array.Sort(names); // Sort
+
+        // Print after Sorting
+        Console.WriteLine("After sorting");
+        foreach (string name in names)
+            Console.Write(name + " ");
+
+        Console.WriteLine(); // New line
+
+        // Print before Reversal 
+        Console.WriteLine("Before reversal");
+        foreach (string name in names)
+            Console.Write(name + " ");
+
+        Console.WriteLine(); // New line
+
+        Array.Reverse(names);
+
+        // Print after Reversal 
+        Console.WriteLine("Before reversal");
+        foreach (string name in names)
+            Console.Write(name + " ");
+            
+        Console.WriteLine(); // New line
     }
 }
--- Topic5A/task1/obj/Debug/net10.0/task1.AssemblyInfo.cs
+++ Topic5A/task1/obj/Debug/net10.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9013bd2ba2762ab7d0e91907ae8ffbd766f65c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7a998aa756e14c29bf76734433388cd4936513d")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
