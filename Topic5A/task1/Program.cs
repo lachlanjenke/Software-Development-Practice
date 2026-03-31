f7a998aa756e14c29bf76734433388cd4936513d
Lachlan Jenke <a1924627@adelaide.edu.au>
2026-03-31 11:05:14 +1030

    Printing array

---
+++ Topic5A/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = {"Noah", "Charlie","Oliver","Amelia","Theo","Mia","Henry","Evelyn","Lachy","Ava"};
+
+        foreach (string name in names)
+            Console.WriteLine(name);
+    }
+}
